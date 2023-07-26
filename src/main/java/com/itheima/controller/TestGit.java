f0db6cc0a894573098af4ad148c7d11f9149fd54
gc0902 <126854721+gc0902@users.noreply.github.com>
2023-07-26 15:42:14 +0800

    Update TestGit.java

--- src/main/java/com/itheima/controller/TestGit.java
+++ src/main/java/com/itheima/controller/TestGit.java
@@ -2,6 +2,6 @@ package com.itheima.controller;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("git!!!!!!!!");
+        System.out.println("byebye!");
     }
 }
